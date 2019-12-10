7d656b8de9452f4e92d6cb0493dc0b4ec1b720f3
Cedrik Razafimanantsoa <cedrik.razafi@gmail.com>
2019-12-10 13:58:14 +0100

    first commit from master

--- Gemfile
+++ Gemfile
@@ -2,11 +2,13 @@ source 'https://rubygems.org'
 ruby '2.6.3'
 
 gem 'bootsnap', require: false
-gem 'jbuilder', '~> 2.0'
+# gem 'jbuilder', '~> 2.0'
 gem 'pg', '~> 0.21'
 gem 'puma'
 gem 'rails', '5.2.3'
 gem 'redis'
+gem 'faker'
+gem 'rest-client'
 
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Garden.destroy_all if Rails.env.development?
+
+garden_names = [
+  "French garden",
+  "My Cute Balcony",
+  "English garden",
+  "Flowered patio",
+  "Garden in Japan",
+  "Walk among flowers"
+]
+6.times do
+  garden_request = RestClient.get("https://source.unsplash.com/1200x700/?garden")
+  garden = Garden.new(
+      name: garden_names.pop,
+      banner_url: garden_request.request.url
+    )
+  garden.save!
+  3.times do
+    plant_request = RestClient.get("https://source.unsplash.com/400x300/?flower")
+    plant = Plant.new(
+      name: Faker::FunnyName.name,
+      image_url: plant_request.request.url
+    )
+    plant.garden = garden
+    plant.save!
+    sleep(2)
+  end
+end
